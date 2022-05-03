cfe718860875b756870a9475583a6578ebb5135c
trezor13 <treasureharden14@gmail.com>
2022-05-03 12:09:40 -0700

    Update TodoAdapter.kt

--- app/src/main/java/com/example/kt_todo/TodoAdapter.kt
+++ app/src/main/java/com/example/kt_todo/TodoAdapter.kt
@@ -3,9 +3,8 @@ package com.example.kt_todo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_todo.view.*
+
 
 class TodoAdapter(
     private val todos: MutableList<Todo>
@@ -22,17 +21,10 @@ class TodoAdapter(
       )
     }
 
-    private fun toggleStrikeThrough(tvTodoTitle: textView, isChecked: Boolean){
-        if(isChecked){
-            tvTodoTitle.paintflags = tvTodoTitle.paintflags
-        }
-    }
-
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
         holder.itemView.apply {
-            tvTodoTitle.tex = curTodo.title
-            cbDone.isChecked = curTodo.isChecked
+            
         }
 
     }
