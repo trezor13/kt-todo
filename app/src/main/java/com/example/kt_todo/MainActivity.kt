582bf9f14a7f2a32135fc7a2a725e2a09c76dfa6
trezor13 <treasureharden14@gmail.com>
2022-05-03 12:53:12 -0700

    wrap up to-do app

--- app/src/main/java/com/example/kt_todo/MainActivity.kt
+++ app/src/main/java/com/example/kt_todo/MainActivity.kt
@@ -1,11 +1,34 @@
-package com.example.kt_todo
+package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kt_todo.R
+import com.example.kt_todo.Todo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
--- app/src/main/java/com/example/kt_todo/Todo.kt
+++ app/src/main/java/com/example/kt_todo/Todo.kt
@@ -2,6 +2,6 @@ package com.example.kt_todo
 
 data class Todo (
     val title:  String,
-    val isChecked: Boolean = false
+    var isChecked: Boolean = false
 
 )
